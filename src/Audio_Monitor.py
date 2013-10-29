b25f6079a7b8ebbdd80866968c87db9e32d7b630
Omer Shapira <info@omershapira.com>
2013-10-29 19:46:22 -0400

    adds buffer class

--- src/Audio_Monitor.py
+++ src/Audio_Monitor.py
@@ -3,19 +3,23 @@ import wave
 import signal
 import sys
 
-CHUNK = 1024
+from array import array
+
+CHUNK = 4096
 FORMAT = pyaudio.paInt16 #TODO: CHeck out what this means
 CHANNELS = 1
 RATE = 44100
 
+# Add signal listener
 def signal_handler(signal, frame):
-        print 'Goodbye'
+        print ('Goodbye')
         sys.exit(0)
+
 signal.signal(signal.SIGINT, signal_handler)
 
-running = True
-
 p = pyaudio.PyAudio()
+buf = ByteBuffer()
+
 stream = p.open(
 	format = FORMAT,
 	channels = CHANNELS,
@@ -24,10 +28,29 @@ stream = p.open(
 	frames_per_buffer = CHUNK
 	)
 
-frames = []
-
-# for i in range(int(RATE/CHUNK *))
-
-while running:
+while True:
 	data = stream.read(CHUNK)
+	buf.add(sum(data)/CHUNK) #integer division
 
+
+class ByteBuffer:
+	"""Buffer for sound objects"""
+	def __init__(self, size, output_file):
+		# FIXME: unpythonic type checking
+		self.size = int(size)
+		self.output_file = str(output_file)
+		# TODO: Allocate output file
+		self.pointer = 0
+
+		self.buffer_array = array('I', [0 for i in xrange(self.size)])
+	
+
+	def add(self,sample):
+		self.buffer_array[self.pointer] = sample
+		self.pointer += 1
+		if self.pointer >= self.size:
+			dump()
+			self.pointer = 0
+
+
+	def dump()
