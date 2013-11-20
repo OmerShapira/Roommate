d128fac6125c843eef2ecae3e9c9c6a81bb2f653
Gal Sasson <sasgal@gmail.com>
2013-11-19 22:08:13 -0500

    added dummyfeed script as an example of creating a session and feeding it data

---
+++ src/dummyfeed/feedata.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#
+# This script will create a new session and will feed it with dummy data
+#
+
+SESSION=$1
+
+if [ -z $SESSION ]
+then
+	echo "USAGE $0 <session>"
+	exit 1
+fi
+
+IP=`cat pi.ip`
+
+# create a new session
+curl -X POST -H "Content-Type: application/json" -d "{ \"name\": \"${SESSION}\" }" "http://${IP}:3000/Session/"
+
+# feed session with dummy data
+for i in {1..100}
+do
+	let time=200+${i}*1000
+	level=${i}
+	content="{ \"updates\" : [ { \"measureName\": \"Sound\", \"value\": ${level}, \"timeStamp\" : ${time}, \"description\" : \"dummy description\" } ] }"
+	echo $content
+	curl -X POST -H "Content-Type: application/json" -d "$content" "http://${IP}:3000/Session/${SESSION}/Update"
+done
+
+echo "done"
+
+
