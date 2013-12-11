b61dcac957fa0a63581e8483173a931f8b2ec04a
Omer Shapira <info@omershapira.com>
2013-12-11 00:10:09 -0500

    new arduino script for motors

--- src/arduino/stepper_1_4_accelstepper_serial/stepper_1_4_accelstepper_serial.ino
+++ src/arduino/stepper_1_4_accelstepper_serial/stepper_1_4_accelstepper_serial.ino
@@ -1,59 +1,78 @@
+#define NUM_STEPPERS
 #include <AccelStepper.h>
 
-// Define two steppers and the pins they will use
-AccelStepper stepper1(1, 9, 8);
-AccelStepper stepper2(1, 7, 6);
+#define SHUT_FAST 0
+#define SHUT_SLOWLY 1
+#define RANDOM_PATTERN1 10
 
-int pos1 = 800;
-int pos2 = 820;
-bool newData = false;
-int value = 0;
+AccelStepper steppers[NUM_STEPPERS] = {
+  AccelStepper(1, 9, 8),
+  AccelStepper(1, 5, 4),
+  AccelStepper(1, 3, 2),
+  AccelStepper(1, 7, 6)};
+int delta[NUM_STEPPERS] = {800, 600, 600, 820}; //TODO: Give 1,2 a real value
+int speed[NUM_STEPPERS] = {3000, 3000, 3000, 2500};
+int acceleration[NUM_STEPPERS] = {1000, 1000, 100, 800};
+
+bool newCommand = false;
+int command = 0;
+int mode = 0;
+
 void setup()
 {  
   Serial.begin(9600);
-  stepper1.setMaxSpeed(3000);
-  stepper1.setAcceleration(1000);
-  stepper2.setMaxSpeed(2500);
-  stepper2.setAcceleration(800);
+  for(int i=0; i<NUM_STEPPERS; i++){
+      steppers[i].setMaxSpeed(speed[i]);
+      steppers[i].setAcceleration(acceleration[i]);
+  }
 }
 
 void loop()
 {
-  if (stepper1.distanceToGo() == 0)
-  {
-    //delay(500);
-    pos1 = -pos1;
-    stepper1.moveTo(pos1);
-  }
-  if (stepper2.distanceToGo() == 0)
-  {
-    //delay(400);
-    pos2 = -pos2;
-    stepper2.moveTo(pos2);
-  }
-  stepper1.run();
-  stepper2.run();
-
-  if(newData){
-    newData = false;
-    if(value ==1){
-      Serial.println("Im in 1");
+  if(newCommand){
+    newCommand = false;
+    switch (command) {
+        case SHUT_FAST:
+          // do something
+          break;
+        case SHUT_SLOWLY:
+          // do something
+          break;
+        case RANDOM_PATTERN1:
+          setMotorsByArrays(true);
+          break;
+        default: break; //Should never happen
     }
-    else if(value == 2){
-      Serial.println("Im in 2");
+  } else {
+    switch (commmand) {
+        case RANDOM_PATTERN1:
+          setMotorsByArrays(false); //Don't force new values
+          break;
+        default:
+          // do something
     }
+  }
 
+  for(int i=0; i<NUM_STEPPERS; i++){
+    steppers[i].run();
   }
 }
 
+void setMotorsByArrays(bool force){
+  for(int i=0; i<NUM_STEPPERS; i++){ //if you're looping
+      if (steppers[i].distanceToGo() == 0){
+        delta[i] *= -1; //HACK: To save Building another array
+      }
+      if (steppers[i].distanceToGo() == 0 || force){
+        steppers[i].moveTo(max(delta[i], 0));
+      }
+  }
+}
 
 void serialEvent() {
   while (Serial.available()) {
-    // get the new byte:
-    //char inChar = (char)Serial.read(); //
-    value = Serial.parseInt();
-    newData = true;
-
+    command = Serial.parseInt();
+    newCommand = true;
   }
 }
 
